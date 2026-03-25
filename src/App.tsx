6fc1a931d1b44f08f79470f9bb80f3572836e7a1
Mayank <mnegi1704@gmail.com>
2026-03-25 21:27:51 +0100

    fixed analytics build

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient();
 
